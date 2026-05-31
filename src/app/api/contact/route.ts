4bcd1e4de64e96a31b8a822c7498630cea9085e2
Aman Bansod <amansb69@gmail.com>
2026-05-31 14:10:04 +0530

    error solved

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+if (!process.env.RESEND_API_KEY) {
+  throw new Error('RESEND_API_KEY is missing');
+}
+
 
 export async function POST(req: NextRequest) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const { name, email, message } = await req.json();
 
